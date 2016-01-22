e5d7b3a1c452f09a9ce73e5d9b74351c8fe2ce86
Rafal Radomski <rafal.radomski@hmhco.com>
2016-01-22 17:07:01 +0000

    tutorial init

---
+++ public/scripts/tutorial2.js
@@ -0,0 +1,20 @@
+var CommentList = React.createClass({
+
+  render: function() {
+    return (
+      <div className=" commentList ">
+        Hello, world! I am a Comment List.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function(){
+   return (
+      <div className=" commentForm ">
+        Hello world! I am Comment Form.
+      </div>
+   );
+  }
+});
