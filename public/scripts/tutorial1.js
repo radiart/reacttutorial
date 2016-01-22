feba04db74ff45bfeecdf2dd5b04a8b42759f36c
Rafal Radomski <rafal.radomski@hmhco.com>
2016-01-22 17:21:09 +0000

    tutorial3

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -2,11 +2,35 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className=" commentBox ">
-        Hello, world! I am a CommentBox.
+      <h1>Comments</h1>
+      <CommentList/>
+      <CommentForm/>
       </div>
     );
   }
 });
+
+var CommentList = React.createClass({
+
+  render: function() {
+    return (
+      <div className=" commentList ">
+      Hello, world! I am a Comment List.
+    </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function(){
+    return (
+      <div className=" commentForm ">
+      Hello world! I am Comment Form.
+    </div>
+    );
+  }
+});
+
 
 ReactDOM.render(
   <CommentBox />,
